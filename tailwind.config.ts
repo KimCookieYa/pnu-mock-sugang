c678c6f396e7edf622598bf30c486d0ae832e130
KimCookieYa <min49590@gmail.com>
2024-02-15 02:24:39 +0900

    style: tailwind 픽셀 단위 지정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,13 @@
 import type { Config } from "tailwindcss";
 
+function generateCSSObject(property: string): Record<string, string> {
+  let cssObject: Record<string, string> = {};
+  for (let i = 1; i <= 2000; i++) {
+    cssObject[`${i}`] = `${i}px`;
+  }
+  return cssObject;
+}
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +16,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        pnuBlue: "#043d92",
       },
+      width: generateCSSObject("width"),
+      height: generateCSSObject("height"),
+      padding: generateCSSObject("padding"),
+      margin: generateCSSObject("margin"),
     },
   },
   plugins: [],
