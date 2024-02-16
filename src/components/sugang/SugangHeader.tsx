6793c8e4e98f55a8df3e3fdad1f9e2c52012004f
KimCookieYa <min49590@gmail.com>
2024-02-17 05:17:49 +0900

    design: 수강페이지 헤더가 테이블에 덮이는 문제 해결

--- src/components/sugang/SugangHeader.tsx
+++ src/components/sugang/SugangHeader.tsx
@@ -3,7 +3,7 @@ import TitleLogo from "../TitleLogo";
 export default function SugangHeader() {
   return (
     <>
-      <header className="flex bg-pnuBlue h-48 items-center px-16 fixed top-0 left-0 right-0 w-full">
+      <header className="flex bg-pnuBlue h-48 items-center px-16 fixed top-0 left-0 right-0 w-full z-[10000]">
         <TitleLogo />
       </header>
       <div className="h-48 w-full" />
