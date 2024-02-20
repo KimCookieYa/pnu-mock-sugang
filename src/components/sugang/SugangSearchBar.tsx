2c4572f54cdcd47284e3fda461ee68ecde181a69
KimCookieYa <min49590@gmail.com>
2024-02-21 03:02:03 +0900

    feat: 빠른 담기 구현

--- src/components/sugang/SugangSearchBar.tsx
+++ src/components/sugang/SugangSearchBar.tsx
@@ -1,10 +1,41 @@
 "use client";
 
+import useLocalStorage from "@/hooks/useLocalStorage";
+import useLoading from "@/stores/loading";
+import { readExcelData } from "@/utils/excel";
+import { generateRandomDelay } from "@/utils/util";
+import { usePathname } from "next/navigation";
 import { useRef } from "react";
 
 export default function SugangSearchBar() {
-  const courceIdRef = useRef<HTMLInputElement>(null);
-  const courceRoomRef = useRef<HTMLInputElement>(null);
+  const pathname = usePathname();
+  const { storedValue, setValue } = useLocalStorage(pathname.slice(1), []);
+  const { setIsLoading } = useLoading();
+  const courseCodeRef = useRef<HTMLInputElement>(null);
+  const courseRoomIdRef = useRef<HTMLInputElement>(null);
+
+  const onSubmitInput = async () => {
+    const data = await readExcelData("/sugang-data-20240124.xlsx");
+    const courseCode = courseCodeRef.current?.value;
+    const courseRoomId = courseRoomIdRef.current?.value;
+    const subject = data?.find(
+      (subject) =>
+        subject.교과목코드 === courseCode && subject.분반 === courseRoomId
+    );
+    if (subject) {
+      setIsLoading(true);
+      setTimeout(() => {
+        setValue([...storedValue, subject]);
+        setIsLoading(false);
+      }, generateRandomDelay());
+    } else {
+      alert("해당 과목이 존재하지 않습니다!");
+      if (courseCodeRef.current && courseRoomIdRef.current) {
+        courseCodeRef.current.value = "";
+        courseRoomIdRef.current.value = "";
+      }
+    }
+  };
 
   return (
     <form className="flex border w-full">
@@ -13,7 +44,7 @@ export default function SugangSearchBar() {
       </label>
       <div className="border p-4 flex justify-center">
         <input
-          ref={courceIdRef}
+          ref={courseCodeRef}
           type="text"
           className="border focus:outline-none"
         />
@@ -23,14 +54,17 @@ export default function SugangSearchBar() {
       </label>
       <div className="border p-4 flex justify-center">
         <input
-          ref={courceRoomRef}
+          ref={courseRoomIdRef}
           type="text"
           className="border focus:outline-none"
         />
       </div>
       <div className="flex justify-center">
-        <button className="bg-pnuWarn text-white text-sm m-6 px-8 py-4 rounded-md">
-          빠른 수강신청
+        <button
+          className="bg-pnuWarn text-white text-sm m-6 px-8 py-4 rounded-md"
+          onClick={onSubmitInput}
+        >
+          {pathname === "/register" ? "빠른 수강신청" : "빠른 담기"}
         </button>
       </div>
     </form>
