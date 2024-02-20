00cb629c5ea32c0209a5b583b7beedf316bbcee8
KimCookieYa <min49590@gmail.com>
2024-02-20 20:54:03 +0900

    fix: table 에러 수정

--- src/app/(main)/notice/page.tsx
+++ src/app/(main)/notice/page.tsx
@@ -1,15 +1,9 @@
 import NoticeSection1 from "@/components/notice/NoriceSection1";
-import Link from "next/link";
 
 export default function NoticePage() {
   return (
     <div className="w-full h-full flex flex-col gap-y-8 items-center">
       <NoticeSection1 />
-      <Link href="/register">
-        <button className="bg-pnuLightBlue text-white px-20 h-fit py-8 rounded text-sm">
-          수강신청 바로가기
-        </button>
-      </Link>
     </div>
   );
 }
--- src/components/notice/NoriceSection1.tsx
+++ src/components/notice/NoriceSection1.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { noticeData } from "@/datas/notice-data";
+import { useRouter } from "next/navigation";
 import { Fragment, useState } from "react";
 
 export default function NoticeSection1() {
+  const router = useRouter();
   const [openIndexList, setOpenIndexList] = useState([true, true, true]);
 
-  // 아코디언 섹션의 데이터 (예시)
-
-  // 행 클릭 핸들러
   const handleRowClick = (index: number) => {
-    // 이미 열려있는 아코디언을 클릭하면 닫고, 그렇지 않으면 열기
-    console.log(index);
     setOpenIndexList((prev) => {
       const newOpenIndexList = [...prev];
       newOpenIndexList[index] = !newOpenIndexList[index];
@@ -19,8 +16,16 @@ export default function NoticeSection1() {
     });
   };
 
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    setTimeout(() => {
+      router.push("/register");
+    }, 3000);
+  };
+
   return (
-    <section className="w-full mb-12">
+    <div className="w-full mb-12 flex flex-col items-center gap-y-12">
       <table className="w-full text-left">
         <tbody>
           {noticeData.map((row, index) => (
@@ -33,21 +38,36 @@ export default function NoticeSection1() {
                   #{index + 1} {row.title}
                 </td>
               </tr>
-              <div
-                className={`overflow-hidden px-60  transition-all duration-500 ease-in-out ${
-                  openIndexList[index] ? "h-100 border-[1px]" : "h-0"
-                }`}
-              >
-                <div className="list-disc mt-16">
-                  {row.content.map((content, index) => (
-                    <li key={index}>{content}</li>
-                  ))}
-                </div>
-              </div>
+              <tr>
+                <td
+                  className={[
+                    openIndexList[index] ? "border-[1px]" : "",
+                    " transition-all duration-500 ease-in-out",
+                  ].join(" ")}
+                >
+                  <div
+                    className={`overflow-hidden px-60 transition-all duration-500 ease-in-out ${
+                      openIndexList[index] ? "h-100" : "h-0"
+                    }`}
+                  >
+                    <div className="list-disc mt-16">
+                      {row.content.map((content, index) => (
+                        <li key={index}>{content}</li>
+                      ))}
+                    </div>
+                  </div>
+                </td>
+              </tr>
             </Fragment>
           ))}
         </tbody>
       </table>
-    </section>
+      <button
+        className="bg-pnuLightBlue text-white px-20 h-fit py-8 rounded text-sm"
+        onClick={handleClick}
+      >
+        수강신청 바로가기
+      </button>
+    </div>
   );
 }
